95a54f81e6e10ef54f9bb36a5049e66597ab540b
Vipin GS <vipings@aol.in>
2017-06-13 19:31:02 +0530

    Adding from Local

--- ex1.py
+++
@@ -1,9 +0,0 @@
-def print_age():
-    name , age = input("Enter your name") , int(input("Enter your age"))
-    
-    for i in range(int(input("Enter a number"))):
-        print("Hello ",name,"!, You will turn 100 in the year",(2017-age)+100," :) \n")
-def main():
-    print_age()
-if __name__ == '__main__':
-    main()
